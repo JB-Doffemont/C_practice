c46febf21cf9cd11b7854d67698c2f196151241e
JB <jeanbernard_doffemont@yahoo.fr>
2022-11-22 08:14:56 +0100

    init hangman_game + function greetings

---
+++ exercices/hangman_game.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hangman_game.h"
+
+int main(int argc, char *argv[])
+{
+    display_greetings();
+    return 0;
+}
+
+void display_greetings()
+{
+
+    printf("Bienvenue dans le Pendu !");
+}
