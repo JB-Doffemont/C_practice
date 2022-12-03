49ea6fa5e7e2163b7e939bbb266978139efeb33d
JB <jeanbernard_doffemont@yahoo.fr>
2022-12-03 14:20:22 +0100

    function greetings (work in progress)

--- exercices/hangman_game.c
+++ exercices/hangman_game.c
@@ -1,12 +1,15 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <ctype.h>
 #include "hangman_game.h"
 
 int main(int argc, char *argv[])
 {
-    char secret_word[] = "MARRON";
-    display_greetings();
+    char secret_word[7] = "MARRON";
+    int number_try = 10;
+
+    display_greetings(secret_word, number_try);
 
     char my_letter = 0;
 
@@ -16,10 +19,19 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void display_greetings()
+void display_greetings(char secret_word[], int number_try)
 {
+    int sizeArray = 0;
+    sizeArray = strlen(secret_word);
 
+    for (int i = 0; i < sizeArray; i++)
+    {
+        secret_word[i] = *"*";
+    }
     printf("Bienvenue dans le Pendu !\n");
+    printf("Quel est le mot secret ? %s\n", secret_word);
+    printf("Il vous reste %i coups a jouer\n", number_try);
+    printf("Proposez une lettre :\n");
 }
 
 char read_character()
