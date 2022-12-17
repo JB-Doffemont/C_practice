609dcf6facd7420bb7c26970f508737e4eaa9e09
JB <jeanbernard_doffemont@yahoo.fr>
2022-12-17 12:42:09 +0100

    main logic display letter after guess (ok)

--- exercices/hangman_game.c
+++ exercices/hangman_game.c
@@ -11,28 +11,48 @@ int main(int argc, char *argv[])
 
     display_greetings(secret_word, number_try);
 
-    char my_letter = 0;
-
-    my_letter = read_character();
-    printf("%c", my_letter);
-
     return 0;
 }
 
-void display_greetings(char secret_word[], int number_try)
+void main_logic(char secret_word[], int number_try)
 {
     int sizeArray = 0;
+    char my_letter = 0;
+    char secret_word_hide[strlen(secret_word)];
+
     sizeArray = strlen(secret_word);
 
     for (int i = 0; i < sizeArray; i++)
     {
-        secret_word[i] = *"*";
+        secret_word_hide[i] = '*';
     }
+
     printf("Bienvenue dans le Pendu !\n");
-    printf("Quel est le mot secret ? %s\n", secret_word);
-    printf("Il vous reste %i coups a jouer\n", number_try);
-    printf("Proposez une lettre :\n");
-}
+
+    do
+    {
+        printf("Quel est le mot secret ? %s\n", secret_word_hide);
+        printf("Il vous reste %i coups a jouer\n", number_try);
+        printf("Proposez une lettre :\n");
+
+        my_letter = read_character();
+        printf("%c\n", my_letter);
+
+        for (int i = 0; i < sizeArray; i++)
+        {
+
+            if (secret_word[i] == my_letter)
+            {
+                secret_word_hide[i] = my_letter;
+            }
+            else if (secret_word[i] != my_letter)
+            {
+                printf("La lettre n'est pas dans le mot !");
+                number_try--;
+            }
+        }
+    } while (secret_word != secret_word_hide);
+};
 
 char read_character()
 {
--- exercices/hangman_game.h
+++ exercices/hangman_game.h
@@ -1,3 +1,4 @@
 
-void display_greetings();
+void main_logic(char secret_word[], int number_try);
 char read_character();
+// void check_character();
